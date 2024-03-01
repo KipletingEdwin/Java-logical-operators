972b7621760737597fd69d69c760cf111e9375bf
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-03-01 11:30:16 +0300

    second commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+       //logical opearator = use to connect two or more expressions
+
+        int temp = 17;
+
+        if (temp>30){
+            System.out.println("It is hot outside");
+        } else if (temp>=20 && temp<=30) {
+            System.out.println("It is warm outside");
+        }
+        else{
+            System.out.println("It is cold outside");
+        }
+    }
+    }
